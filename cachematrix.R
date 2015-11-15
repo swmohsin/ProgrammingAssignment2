e7c383801ecde4e76f4d4f84fbb752ce5410f2cd
Wasi <wasi@algotrek.com>
2015-11-15 15:22:38 -0500

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## this function takes a matrix object as a parameter and returns a list of functions
+## this function is used internally by the next function to implement caching of inverse matrix operation (in case matrix does not change)
+## you are supposed to use these functions like this:
+## assume x is a matrix object (which is invertable)
+## mat <- makeCacheMatrix(x)
+## cacheSolve(mat)
 
 makeCacheMatrix <- function(x = matrix()) {
 	i <- NULL
@@ -16,7 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## this function calculates the inverse of a matrix and stores it in an object
+## next time this function is called with the same matrix, it does not recalculate the inveres. instead, it just returns the values from its 'cache'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
